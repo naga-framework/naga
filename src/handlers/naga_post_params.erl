3947e16e2cada2f991644ee739a59ce4028b85bb
mihawk <chan.sisowath@gmail.com>
2016-06-23 11:58:49 +0800

    check multipart

--- src/handlers/naga_post_params.erl
+++ src/handlers/naga_post_params.erl
@@ -2,9 +2,14 @@
 -include_lib("n2o/include/wf.hrl").
 -export([init/2,finish/2]).
 
+%%FIXME: multipart/form-data
 init(_State, Ctx) ->
-    {Form,NewReq} = wf:form(Ctx#cx.req),
-    NewCtx = Ctx#cx{form=Form,req=NewReq},
-    {ok, [], NewCtx}.
+	case cowboy_req:parse_header(<<"content-type">>, Ctx#cx.req) of
+	 {ok, {<<"multipart">>, <<"form-data">>, _}, Req} -> 
+	 				 {ok, [], Ctx#cx{form=multipart,req=Req}};
+     {ok, _, Req} -> {Form,NewReq} = wf:form(Req),
+    				 NewCtx = Ctx#cx{form=Form,req=NewReq},
+                     {ok, [], NewCtx} 
+    end.
 
 finish(_State, Ctx) ->  {ok, [], Ctx}.
